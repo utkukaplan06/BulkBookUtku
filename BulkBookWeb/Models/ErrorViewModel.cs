6faaba87502165fcb61590661a054332192b86bb
utkukaplan06 <63205557+utkukaplan06@users.noreply.github.com>
2023-02-13 19:12:12 +0300

    Add project files.

---
+++ BulkBookWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BulkBookWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
