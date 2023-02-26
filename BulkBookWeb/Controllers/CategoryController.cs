0b0eb008e6cff4fb2db1a0af6f580740ddcc2147
utkukaplan06 <63205557+utkukaplan06@users.noreply.github.com>
2023-02-26 20:34:35 +0300

    36 server side validations

--- BulkBookWeb/Controllers/CategoryController.cs
+++ BulkBookWeb/Controllers/CategoryController.cs
@@ -29,9 +29,14 @@ namespace BulkBookWeb.Controllers
         [ValidateAntiForgeryToken]
         public IActionResult Create(Category obj)
         {
-            _db.Categories.Add(obj);
-            _db.SaveChanges();
-            return RedirectToAction("Index");
+            if(ModelState.IsValid)
+            {
+                _db.Categories.Add(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(obj);
         }
     }
 }
