d62e3ba44646d2eafdcde5314e1ff00a248fe9bf
utkukaplan06 <63205557+utkukaplan06@users.noreply.github.com>
2023-02-28 11:11:47 +0300

    Range validation

--- BulkBookWeb/Models/Category.cs
+++ BulkBookWeb/Models/Category.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace BulkBookWeb.Models
 {
@@ -10,7 +11,9 @@ namespace BulkBookWeb.Models
         [Required]
         public string Name { get; set; }
 
-        public string DisplayOrder { get; set; }
+		[DisplayName("Display Order")]
+		[Range(1, 100, ErrorMessage = "Display Order must be between 1 and 100 only!!")]
+		public string DisplayOrder { get; set; }
 
         public DateTime CreatedDateTime { get; set; } = DateTime.Now;
     }
