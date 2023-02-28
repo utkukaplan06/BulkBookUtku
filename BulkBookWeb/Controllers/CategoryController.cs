06fc542c154107245903a9b0d41abc7d54c7403c
utkukaplan06 <63205557+utkukaplan06@users.noreply.github.com>
2023-02-28 19:15:24 +0300

    Partial views

--- BulkBookWeb/Controllers/CategoryController.cs
+++ BulkBookWeb/Controllers/CategoryController.cs
@@ -33,6 +33,7 @@ namespace BulkBookWeb.Controllers
             {
                 _db.Categories.Add(obj);
                 _db.SaveChanges();
+                TempData["success"] = "Category created successfully";
                 return RedirectToAction("Index");
             }
 
@@ -71,7 +72,8 @@ namespace BulkBookWeb.Controllers
 			{
                 _db.Categories.Update(obj);
                 _db.SaveChanges();
-				return RedirectToAction("Index");
+                TempData["success"] = "Category updated successfully";
+                return RedirectToAction("Index");
 			}
 			return View(obj);
 		}
@@ -107,8 +109,8 @@ namespace BulkBookWeb.Controllers
 
             _db.Categories.Remove(obj);
             _db.SaveChanges();
+            TempData["success"] = "Category deleted successfully";
             return RedirectToAction("Index");
-
         }
     }
 }
